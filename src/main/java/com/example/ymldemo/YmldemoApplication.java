d5af349f41c913148e5bb0cfd26c42394b4f4d17
m4rchen <snea_kers@naver.com>
2019-10-30 01:00:13 +0900

    Initial commit

---
+++ src/main/java/com/example/ymldemo/YmldemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ymldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmldemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmldemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ymldemo/YmldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ymldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmldemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
