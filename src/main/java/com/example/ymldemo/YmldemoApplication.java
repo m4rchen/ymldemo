9ff8ea345e8bdb30372a56bccda615f34df7a0d0
m4rchen <snea_kers@naver.com>
2019-10-30 23:17:10 +0900

    JPA training

--- src/main/java/com/example/ymldemo/YmldemoApplication.java
+++ src/main/java/com/example/ymldemo/YmldemoApplication.java
@@ -2,7 +2,9 @@ package com.example.ymldemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(value = "com.example.ymldemo.user")
 @SpringBootApplication
 public class YmldemoApplication {
 
